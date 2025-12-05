d5f7ec157eb52b06b1a81525960c811f8fe58730
Lasisi Ibrahim <113442282+ibrahimpelumi6142@users.noreply.github.com>
2025-12-05 17:12:01 +0000

    Create page.tsx

---
+++ app/tools/instagram-downloader/page.tsx
@@ -0,0 +1,8 @@
+export default function InstagramDownloaderPage() {
+  return (
+    <div>
+      <h1>Instagram Downloader</h1>
+      <p>This is a placeholder UI for the Instagram downloader.</p>
+    </div>
+  );
+}
